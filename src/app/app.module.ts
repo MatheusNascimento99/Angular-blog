57a82e465dd908edb89e7f06fa439d7757c5a755
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-01-02 11:03:18 -0300

    ciração title e big card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmllCardComponent } from './components/smll-card/smll-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SmllCardComponent } from './components/smll-card/smll-card.component';
     MenuBarComponent,
     MenuTitleComponent,
     BigCardComponent,
-    SmllCardComponent
+    SmllCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
