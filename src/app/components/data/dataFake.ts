3081e480eb04faab6a2ca6897e20b3477994cbdc
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-01-03 12:11:46 -0300

    finalização do estilo e alinhamento do conteúdo

--- src/app/components/data/dataFake.ts
+++ src/app/components/data/dataFake.ts
@@ -1,13 +1,55 @@
 export const dataFake = [
-    {
-        "id": "1",
-        "title":"VENOM anuncia liga maligna para entretenimento",
-        "description": "Elel disse: é hora de  bla ba bla bla bla la bla vlal",
-        "photoCover":"https://t4.ftcdn.net/jpg/05/61/76/63/360_F_561766327_EYOqZsRt12K2teNV7i3sfZ0FFPNngHS2.jpg"
-    },
-    {"id": "2",
-    "title":"titulo 2",
-    "description": "descrição 2",
-    "photoCover":""
-}
+  {
+    id: '1',
+    title: 'VENOM anuncia nova Aliança do Mal',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi bibendum urna ut neque aliquet, ac aliquam lectus faucibus. 
+    Vestibulum varius sem vel dui dignissim, in auctor diam congue. Morbi non faucibus est. Nunc nec augue aliquam, condimentum enim sed, 
+    convallis quam. Phasellus in libero purus. Morbi dictum augue ornare lacus elementum rutrum. Donec sed gravida lectus. Proin ut rutrum urna. 
+    Aenean vel tortor in quam interdum pharetra nec at nulla. Vivamus sagittis ligula ac consequat ullamcorper. Nulla maximus mattis leo, 
+    non auctor magna tincidunt sed. Phasellus justo ante, pellentesque at convallis sed, feugiat sed nunc.\n\n
+    Phasellus malesuada non magna et pharetra. Quisque laoreet consequat turpis, id auctor libero molestie ut. In dignissim elit id nunc ornare, 
+    ut ultricies sem iaculis. Mauris scelerisque, eros scelerisque imperdiet gravida, elit neque luctus nisi, a efficitur nibh dolor sed erat. 
+    Duis commodo facilisis blandit. Nam eu urna sed libero aliquam posuere. Donec eu sollicitudin mi, ut dapibus nulla. Maecenas nec consequat quam, 
+    sit amet egestas enim. Vivamus hendrerit placerat turpis in varius.`,
+    photoCover:'https://pop.proddigital.com.br/wp-content/uploads/sites/8/2021/07/venom-1.jpg',
+  },
+  { id: '2', 
+  title: ' Vilão muito esquecido ou pouco lembrado?', 
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi bibendum urna ut neque aliquet, ac aliquam lectus faucibus. 
+  Vestibulum varius sem vel dui dignissim, in auctor diam congue. Morbi non faucibus est. Nunc nec augue aliquam, condimentum enim sed, 
+  convallis quam. Phasellus in libero purus. Morbi dictum augue ornare lacus elementum rutrum. Donec sed gravida lectus. Proin ut rutrum urna. 
+  Aenean vel tortor in quam interdum pharetra nec at nulla. Vivamus sagittis ligula ac consequat ullamcorper. Nulla maximus mattis leo, 
+  non auctor magna tincidunt sed. Phasellus justo ante, pellentesque at convallis sed, feugiat sed nunc.\n\n
+  Phasellus malesuada non magna et pharetra. Quisque laoreet consequat turpis, id auctor libero molestie ut. In dignissim elit id nunc ornare, 
+  ut ultricies sem iaculis. Mauris scelerisque, eros scelerisque imperdiet gravida, elit neque luctus nisi, a efficitur nibh dolor sed erat. 
+  Duis commodo facilisis blandit. Nam eu urna sed libero aliquam posuere. Donec eu sollicitudin mi, ut dapibus nulla. Maecenas nec consequat quam, 
+  sit amet egestas enim. Vivamus hendrerit placerat turpis in varius.`, 
+  photoCover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN2TixhPBrXFZMF2h6cQmAzc8AUY5p3IWIVg&usqp=CAU'
+ },
+ { id: '3', 
+ title: 'Desdenho total de vilã!', 
+ description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi bibendum urna ut neque aliquet, ac aliquam lectus faucibus. 
+ Vestibulum varius sem vel dui dignissim, in auctor diam congue. Morbi non faucibus est. Nunc nec augue aliquam, condimentum enim sed, 
+ convallis quam. Phasellus in libero purus. Morbi dictum augue ornare lacus elementum rutrum. Donec sed gravida lectus. Proin ut rutrum urna. 
+ Aenean vel tortor in quam interdum pharetra nec at nulla. Vivamus sagittis ligula ac consequat ullamcorper. Nulla maximus mattis leo, 
+ non auctor magna tincidunt sed. Phasellus justo ante, pellentesque at convallis sed, feugiat sed nunc.\n\n
+ Phasellus malesuada non magna et pharetra. Quisque laoreet consequat turpis, id auctor libero molestie ut. In dignissim elit id nunc ornare, 
+ ut ultricies sem iaculis. Mauris scelerisque, eros scelerisque imperdiet gravida, elit neque luctus nisi, a efficitur nibh dolor sed erat. 
+ Duis commodo facilisis blandit. Nam eu urna sed libero aliquam posuere. Donec eu sollicitudin mi, ut dapibus nulla. Maecenas nec consequat quam, 
+ sit amet egestas enim. Vivamus hendrerit placerat turpis in varius.`,
+ photoCover: 'https://conteudo.imguol.com.br/c/entretenimento/ee/2018/05/29/cena-do-filme-malevola-2014-1527619412265_v2_900x506.jpg'
+},
+{ id: '4', 
+title: 'Empresário de sucesso!', 
+description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi bibendum urna ut neque aliquet, ac aliquam lectus faucibus. 
+Vestibulum varius sem vel dui dignissim, in auctor diam congue. Morbi non faucibus est. Nunc nec augue aliquam, condimentum enim sed, 
+convallis quam. Phasellus in libero purus. Morbi dictum augue ornare lacus elementum rutrum. Donec sed gravida lectus. Proin ut rutrum urna. 
+Aenean vel tortor in quam interdum pharetra nec at nulla. Vivamus sagittis ligula ac consequat ullamcorper. Nulla maximus mattis leo, 
+non auctor magna tincidunt sed. Phasellus justo ante, pellentesque at convallis sed, feugiat sed nunc.\n\n
+Phasellus malesuada non magna et pharetra. Quisque laoreet consequat turpis, id auctor libero molestie ut. In dignissim elit id nunc ornare, 
+ut ultricies sem iaculis. Mauris scelerisque, eros scelerisque imperdiet gravida, elit neque luctus nisi, a efficitur nibh dolor sed erat. 
+Duis commodo facilisis blandit. Nam eu urna sed libero aliquam posuere. Donec eu sollicitudin mi, ut dapibus nulla. Maecenas nec consequat quam, 
+sit amet egestas enim. Vivamus hendrerit placerat turpis in varius.`,
+photoCover: 'https://veja.abril.com.br/wp-content/uploads/2016/05/o-vilao-de-harry-potter-lord-voldemort-foi-vivido-pelo-ator-ralph-fiennes-620-original.jpeg?quality=90&strip=info'
+},
 ];
